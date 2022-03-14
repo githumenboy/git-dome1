c2e5e14e33531473008285f57f6404206eaa09d9
luozhen <2264147636@qq.com>
2022-03-14 18:55:50 +0800

    hot-fix commit1

--- src/main/java/com/luozhen/git/GitText.java
+++ src/main/java/com/luozhen/git/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello gt");
+        System.out.println("hello gt");
     }
 }
