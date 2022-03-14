41b143a4bd5821cb3d08094d696b795442fc4e2a
luozhen <2264147636@qq.com>
2022-03-14 17:23:32 +0800

    second commit

--- src/main/java/com/luozhen/git/GitText.java
+++ src/main/java/com/luozhen/git/GitText.java
@@ -3,5 +3,6 @@ package com.luozhen.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello gt");
     }
 }
